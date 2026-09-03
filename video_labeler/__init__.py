463027d2632bdaa779cc318fd3790700bcaf5dd9
wjh941 <1610216853@qq.com>
2026-09-03 11:12:28 +0800

    fix: expose current schema version

--- video_labeler/__init__.py
+++ video_labeler/__init__.py
@@ -2,8 +2,9 @@
 
 from .domain import Event, Evidence, MediaAsset, Person, Prediction, Sample
 from .quality import QualityIssue, QualityReport, dataset_stats, export_jsonl, validate_dataset
+from .schema import CURRENT_SCHEMA_VERSION
 
 __all__ = [
     "Event", "Evidence", "MediaAsset", "Person", "Prediction", "Sample",
-    "QualityIssue", "QualityReport", "dataset_stats", "export_jsonl", "validate_dataset",
+    "QualityIssue", "QualityReport", "dataset_stats", "export_jsonl", "validate_dataset", "CURRENT_SCHEMA_VERSION",
 ]
